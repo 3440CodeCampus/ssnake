39296c15da38b8aa304b67ca13f457d16a43f5ae
mtlgdroid <mtlgdroid@users.noreply.github.com>
2019-03-31 10:47:49 -0400

    Game over when hit the wall

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -25,8 +25,8 @@ int Board::getGridHeight() const
 
 bool Board::IsInsideBoard(const Location& loc) const
 {
-	return loc.x +10  >= 10 && loc.x < gfx.ScreenWidth - 10 &&
-		loc.y +10 >= 10 && loc.y < gfx.ScreenHeight - 10;
+	return loc.x *20 >= 20 && loc.x*20 < gfx.ScreenWidth - 25 &&
+		loc.y *20 >= 20 && loc.y*20 < gfx.ScreenHeight - 25;
 
 	// return loc.x >= 0 && loc.x < width &&
 	//	loc.y >= 0 && loc.y < height;
@@ -38,19 +38,19 @@ void Board::DrawWall() const
 	int scrHeight = gfx.ScreenHeight;
 	int thick = 0;
 	do {
-		for (int x = 10; x <= scrWidth - 10; x++)
+		for (int x = 20; x <= scrWidth - 20; x++)
 			{
-				gfx.PutPixel(x, 5+thick, 152, 234, 230);
-				gfx.PutPixel(x, 585 + thick, 152, 234, 230);
+				gfx.PutPixel(x, 20+thick, 152, 234, 230);
+				gfx.PutPixel(x, 575 + thick, 152, 234, 230);
 			}
 		thick++;
 	} while (thick <= 5);
 	thick = 0;
 	do {
-		for (int y = 10; y <= scrHeight - 10; y++)
+		for (int y = 20; y <= scrHeight - 20; y++)
 		{
-			gfx.PutPixel(10+thick, y, 152, 234, 230);
-			gfx.PutPixel(785+thick, y, 152, 234, 230);
+			gfx.PutPixel(20+thick, y, 152, 234, 230);
+			gfx.PutPixel(775+thick, y, 152, 234, 230);
 		}
 		thick++;
 	} while (thick <= 5);
